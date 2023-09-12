865b087e259362bb3854f88eb8739177565a5d98
vission <vission8899@gmail.com>
2023-09-12 08:34:30 +0800

    init

---
+++ src/main/java/top/vission/randompicture/RandomPictureApplication.java
@@ -0,0 +1,15 @@
+package top.vission.randompicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author vission.lu
+ */
+@SpringBootApplication
+public class RandomPictureApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RandomPictureApplication.class, args);
+  }
+}
---
+++ src/test/java/top/vission/randompicture/RandomPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package top.vission.randompicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomPictureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
